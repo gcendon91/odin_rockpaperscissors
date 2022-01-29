830ca73f364b9ce65b969b4a51d3e0076984b266
Gonzalo Cendón <gonzacendon@gmail.com>
2022-01-29 14:46:50 -0300

    Funcion principal agregada, ya es funcional. Faltan detalles y mejoras

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function computerPlay() {
     var result = ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
     
-    return console.log(result.toLowerCase());
+    return result;
 }
 
 
@@ -9,40 +9,76 @@ function letsPlay(playerSelection, computerSelection){
 
     if(playerSelection==='rock'&&computerSelection==='scissors')
     {
-        return console.log("El jugador gana")
+        return 1
     }
     else if(playerSelection==='scissors'&&computerSelection==='paper')
     {
-        return console.log("El jugador gana")
+        return 1
     }
     else if(playerSelection==='paper'&&computerSelection==='rock')
     {
-        return console.log("El jugador gana")
+        return 1
     }
     else if(playerSelection==='paper'&&computerSelection==='scissors')
     {
-        return console.log("La maquina gana")
+        return 2
     }
     else if(playerSelection==='rock'&&computerSelection==='paper')
     {
-        return console.log("La maquina gana")
+        return 2
     }
     else if(playerSelection==='scissors'&&computerSelection==='rock')
     {
-        return console.log("La maquina gana")
+        return 2
     }
     else if(playerSelection===computerSelection)
     {
         return console.log("Empate")
     }
+    else
+    {
+        return console.log("Elección invalida")
+    }
     
 }
 
 function game(){
-    for(let i= 0; i<5; i++)
+    let wins = 0;
+    let losses = 0;
+    for(let i= 1; i<6; i++)
     {
-        let usuario = prompt('Rock, Scissors or Paper? Choose wisely');
-        usuario = usuario.toLowerCase();
-        letsPlay(usuario, computerPlay());
+        let ronda = i;
+        let usuario = prompt('Ronda ' + ronda + '\nRock, Scissors or Paper? Choose wisely');
+        usuario = usuario.toLowerCase();        
+        
+        let maquina = computerPlay();
+        maquina = maquina.toLowerCase();
+        alert("El usuario eligio: " + usuario + " La maquina eligio: " + maquina);
+        
+        let result = letsPlay(usuario, maquina);
+        if(result === 1){
+            wins++;
+            alert("Bien, segui asi!");
+        }
+        else if (result === 2)
+        {
+            losses++;
+            alert("Te ganó, proba de nuevo");
+        }
+        else{
+            alert("Eligieron lo mismo");
+        }
+        
+    }
+    alert("El resultado es usuario: " + wins + " La maquina: " +losses);
+    if(wins>losses)
+    {
+        alert("Ganaste!");
+    }
+    else if (wins<losses){
+        alert("Perdiste!");
+    }
+    else{
+        alert("Empate!");
     }
 }
