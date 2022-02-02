9318195039cc123bac91e7524d85dd96c57ee92c
Gonzalo Cendón <gonzacendon@gmail.com>
2022-02-02 15:13:36 -0300

    Agregada UI y estilos CSS

--- script.js
+++ script.js
@@ -1,84 +1,117 @@
+let wins = 0;
+let losses = 0;
+
+const buttons = document.querySelectorAll(".button")
+const resetBtn= document.querySelector(".reset");
+resetBtn.addEventListener('click',() => location.reload());
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        letsPlay(button.id);
+    })
+})
 function computerPlay() {
     var result = ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
-    
+
     return result;
 }
 
+function letsPlay(playerSelection) {
 
-function letsPlay(playerSelection, computerSelection){
+    let computerSelection = computerPlay();
+    let computerSelectionUndercase = computerSelection.toLowerCase();
 
-    if(playerSelection==='rock'&&computerSelection==='scissors')
-    {
-        return 1
+    if (playerSelection === 'rock' && computerSelectionUndercase === 'scissors') {
+        wins++;
+        document.querySelector(".result").innerText = "You won! you chose " + playerSelection + " and the machine chose " + computerSelectionUndercase;
+        document.querySelector(".score").innerText = "Your score: " + wins + "Machine score: " + losses;
+
     }
-    else if(playerSelection==='scissors'&&computerSelection==='paper')
-    {
-        return 1
+    else if (playerSelection === 'scissors' && computerSelectionUndercase === 'paper') {
+        wins++;
+        document.querySelector(".result").innerText = "You won! you chose " + playerSelection + " and the machine chose " + computerSelectionUndercase;
+
+        document.querySelector(".score").innerText = "Your score: " + wins + " Machine score: " + losses;
     }
-    else if(playerSelection==='paper'&&computerSelection==='rock')
-    {
-        return 1
+    else if (playerSelection === 'paper' && computerSelectionUndercase === 'rock') {
+        wins++;
+        document.querySelector(".result").innerText = "You won! you chose " + playerSelection + " and the machine chose " + computerSelectionUndercase;
+
+        document.querySelector(".score").innerText = "Your score: " + wins + " Machine score: " + losses;
     }
-    else if(playerSelection==='paper'&&computerSelection==='scissors')
-    {
-        return 2
+    else if (playerSelection === 'paper' && computerSelectionUndercase === 'scissors') {
+        losses++;
+        document.querySelector(".result").innerText = "You lose! you chose " + playerSelection + " and the machine chose " + computerSelectionUndercase;
+
+        document.querySelector(".score").innerText = "Your score: " + wins + " Machine score: " + losses;
     }
-    else if(playerSelection==='rock'&&computerSelection==='paper')
-    {
-        return 2
+    else if (playerSelection === 'rock' && computerSelectionUndercase === 'paper') {
+        losses++;
+        document.querySelector(".result").innerText = "You lose! you chose " + playerSelection + " and the machine chose " + computerSelectionUndercase;
+
+        document.querySelector(".score").innerText = "Your score: " + wins + " Machine score: " + losses;
     }
-    else if(playerSelection==='scissors'&&computerSelection==='rock')
-    {
-        return 2
+    else if (playerSelection === 'scissors' && computerSelectionUndercase === 'rock') {
+        losses++;
+        document.querySelector(".result").innerText = "You lose! you chose " + playerSelection + " and the machine chose " + computerSelectionUndercase;
+
+        document.querySelector(".score").innerText = "Your score: " + wins + " Machine score: " + losses;
     }
-    else if(playerSelection===computerSelection)
-    {
-        return console.log("Empate")
+    else if (playerSelection === computerSelectionUndercase) {
+        document.querySelector(".result").innerText = "It's a tie! you chose " + playerSelection + " and the machine chose " + computerSelectionUndercase;
+
+        document.querySelector(".score").innerText = "Your score: " + wins + " Machine score: " + losses;
     }
-    else
-    {
-        return console.log("Elección invalida")
+    checkScore(wins, losses);
+}
+function checkScore(wins, losses) {
+    if (wins === 5) {
+        buttons.forEach(button => button.disabled = true);
+        document.querySelector(".h2").innerText = "You won!!! Congrats!!!";
     }
-    
+    else if (losses === 5) {
+        buttons.forEach(button => button.disabled = true);
+        document.querySelector(".h2").innerText = "You lost, better luck next time!";
+    }
 }
 
-function game(){
-    let wins = 0;
-    let losses = 0;
-    for(let i= 1; i<6; i++)
-    {
-        let ronda = i;
-        let usuario = prompt('Ronda ' + ronda + '\nRock, Scissors or Paper? Choose wisely');
-        usuario = usuario.toLowerCase();        
+
+// function game(){
+//     let wins = 0;
+//     let losses = 0;
+//     for(let i= 1; i<6; i++)
+//     {
+//         let ronda = i;
+//         let usuario = prompt('Ronda ' + ronda + '\nRock, Scissors or Paper? Choose wisely');
+//         usuario = usuario.toLowerCase();        
         
-        let maquina = computerPlay();
-        maquina = maquina.toLowerCase();
-        alert("El usuario eligio: " + usuario + " La maquina eligio: " + maquina);
+//         let maquina = computerPlay();
+//         maquina = maquina.toLowerCase();
+//         alert("El usuario eligio: " + usuario + " La maquina eligio: " + maquina);
         
-        let result = letsPlay(usuario, maquina);
-        if(result === 1){
-            wins++;
-            alert("Bien, segui asi!");
-        }
-        else if (result === 2)
-        {
-            losses++;
-            alert("Te ganó, proba de nuevo");
-        }
-        else{
-            alert("Eligieron lo mismo");
-        }
+//         let result = letsPlay(usuario, maquina);
+//         if(result === 1){
+//             wins++;
+//             alert("Bien, segui asi!");
+//         }
+//         else if (result === 2)
+//         {
+//             losses++;
+//             alert("Te ganó, proba de nuevo");
+//         }
+//         else{
+//             alert("Eligieron lo mismo");
+//         }
         
-    }
-    alert("El resultado es usuario: " + wins + " La maquina: " +losses);
-    if(wins>losses)
-    {
-        alert("Ganaste!");
-    }
-    else if (wins<losses){
-        alert("Perdiste!");
-    }
-    else{
-        alert("Empate!");
-    }
-}
+//     }
+//     alert("El resultado es usuario: " + wins + " La maquina: " +losses);
+//     if(wins>losses)
+//     {
+//         alert("Ganaste!");
+//     }
+//     else if (wins<losses){
+//         alert("Perdiste!");
+//     }
+//     else{
+//         alert("Empate!");
+//     }
+// }
